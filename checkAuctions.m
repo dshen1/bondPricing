6d860caaaade413e37b3cba6509b4e7dc585fa72
cgroll <groll.christian.edu@gmail.com>
2016-07-21 10:29:42 +0200

    auction dates matching analysis

--- checkAuctions.m
+++ checkAuctions.m
@@ -11,6 +11,9 @@ realAuctions.Date = datenum(realAuctions.AuctionDate);
 % for each security find the closest matching synthetic one
 allTypes = unique(realAuctions.SecurityType);
 
+% preallocation
+matchedSecurities = [];
+
 for ii=1:length(allTypes)
     thisType = allTypes{ii};
     
@@ -18,17 +21,39 @@ for ii=1:length(allTypes)
     thisTypeReal = selRowsProp(realAuctions, 'SecurityType', thisType);
     thisTypeSynth = selRowsProp(synthAuctions, 'Name', thisType);
 
+    % find matching synthetic securities
+    thisMatch = thisTypeReal(:, 1:3); % preallocation
+
+    for jj=1:size(thisTypeReal, 1)
+        thisDate = thisTypeReal.Date(jj);
+        
+        [~, I] = min(abs(thisTypeSynth.AuctionDate - thisDate));
+        
+        thisMatch(jj, :) = thisTypeSynth(I, {'Name', 'Weekday', 'Date'});
+    end
+    
+    % fix column names
+    thisMatch.Properties.VariableNames = {'Name', 'Synth_Weekday', 'Synth_Date'};
+    
+    % attach matched synthetic securities
+    thisTypeReal = [thisTypeReal thisMatch(:, 2:end)];
+    
+    % collect results
+    matchedSecurities = [matchedSecurities; thisTypeReal];
 end
 
-%% find closest match
+matchedSecurities.Synth_NumDate = datenum(matchedSecurities.Synth_Date, 'mmm-dd-yyyy');
 
-thisMatch = thisTypeReal(:, 1:3);
+% sort columns
+matchedSecurities = matchedSecurities(:, {'Date', 'Synth_NumDate', 'SecurityType', 'Weekday', ...
+    'Synth_Weekday', 'AuctionDate', 'Synth_Date'});
 
-for ii=1:size(thisTypeReal, 1)
-    thisDate = thisTypeReal.Date;
-    
-    [~, I] = min(abs(thisTypeSynth.AuctionDate - thisDate));
-    
-    thisMatch(ii, :) = thisTypeSynth(I, {'WeekDay', 'Date'})
+%% check matching frequencies
 
-end
+xxPerfectMatch = matchedSecurities.Date == matchedSecurities.Synth_NumDate;
+
+% get matching frequencies
+matchFreq = sum(xxPerfectMatch) / size(matchedSecurities, 1);
+
+% inspect miss-machted securities
+missMatched = matchedSecurities(~xxPerfectMatch, :);
