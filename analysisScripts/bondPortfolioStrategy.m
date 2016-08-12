a1889a6809d3666d71c39a451bcb928cdeae900f
cgroll <groll.christian.edu@gmail.com>
2016-08-12 16:25:47 +0200

    strategy should work now

--- analysisScripts/bondPortfolioStrategy.m
+++ analysisScripts/bondPortfolioStrategy.m
@@ -1,165 +1,70 @@
-% bond portfolio strategy
+% % bond portfolio strategy
+% % 
+% % The strategy will be to invest in
+% % - notes only:
+% %   - replicating real ETF behavior (almost no weight on bonds)
+% %   - real ETF behavior might only be a snapshot, and in reality notes
+% %     might have been prefered only due to different coupon rates
+% % - maturities between 7 and 10 years
+% % - only notes are taken, and in three month steps
+% % - re-balancing only due to expired notes; intermediate coupon cash-flows
+% %   are not directly re-invested
 % 
-% The strategy will be to invest in
-% - notes only:
-%   - replicating real ETF behavior (almost no weight on bonds)
-%   - real ETF behavior might only be a snapshot, and in reality notes
-%     might have been prefered only due to different coupon rates
-% - maturities between 7 and 10 years
-% - only notes are taken, and in three month steps
-% - re-balancing only due to expired notes; intermediate coupon cash-flows
-%   are not directly re-invested
-
-%% load data
-
-% set data directory
-dataDir = '../priv_bondPriceData';
-fname = fullfile(dataDir, 'syntheticBondsLongFormat.mat');
-load(fname)
-
-%% define strategy parameters
-
-GS = GlobalSettings;
-
-% initial wealth
-initWealth = 10000;
-
-% define initial starting date and move to next business day
-desiredInitDate = datenum('1975-01-02');
-initDate = makeBusDate(desiredInitDate, 'follow', GS.Holidays, GS.WeekendInd);
-
-% define TTM range
-minDur = 7*365 + 2; % exclude 7 year notes
-maxDur = 10*365;
-
-%% restrict observations with regards to chosen backtest period
-% subsetting the data, independent of bond strategy
-
-% get observations within backtest period
-xxInd = longPrices.Date >= initDate;
-btPrices = longPrices(xxInd, :);
-
-%% restrict observations with regards to strategy parameters
-% - get TTM for each observation
-% - eliminate observations outside of TTM range
-% - eliminate 30-Year BONDs
-% - eligible bonds and universe bonds change over time; in other words, a
-%   given bond might be eligible / part of the universe only some part of
-%   its lifetime; hence, eligibility has to be determined on the
-%   granularity given by daily observations
-
-% get time to maturity for each observation
-btPrices = sortrows(btPrices, 'Date');
-btPrices.CurrentMaturity = btPrices.Maturity - btPrices.Date;
-
-% eliminate 30 year bonds
-xxInds = strcmp(btPrices.TreasuryType, '30-Year BOND');
-btPrices = btPrices(~xxInds, :);
-
-% reduce to eligible bonds
-xxEligible = btPrices.CurrentMaturity >= minDur & btPrices.CurrentMaturity <= maxDur;
-btPrices = btPrices(xxEligible, :);
-
-
-%% get eligible cash-flows
-
-% get eligible bonds as Treasury array
-eligibleBondIDs = unique(btPrices.TreasuryID);
-xx = findInKeys(eligibleBondIDs, {allTreasuries.ID}');
-eligibleBonds = allTreasuries(xx);
-
-% get all cash-flow dates as matrix
-pfCfDates = cfdates([eligibleBonds.AuctionDate]', [eligibleBonds.Maturity]', ...
-    {eligibleBonds.Period}', {eligibleBonds.Basis}');
-
-% transform cash-flow dates to table
-pfCfTable = array2table(pfCfDates);
-pfCfTable = [array2table(eligibleBondIDs, 'VariableNames', {'TreasuryID'}) , pfCfTable];
-pfCfTable = stack(pfCfTable, tabnames(pfCfTable(:, 2:end)), ...
-    'NewDataVariableName', 'Date',...
-    'IndexVariableName', 'toDelete');
-pfCfTable = pfCfTable(:, {'TreasuryID', 'Date'});
-pfCfTable = pfCfTable(~isnan(pfCfTable.Date), :);
-pfCfTable = sortrows(pfCfTable, 'Date');
-
-% attach coupon payments
-xxInfoTable = summaryTable(eligibleBonds);
-pfCfTable = outerjoin(pfCfTable, xxInfoTable(:, {'TreasuryID', 'CouponRate'}), ...
-    'Keys', 'TreasuryID', 'MergeKeys', true, 'Type', 'left');
-pfCfTable.Properties.VariableNames{'CouponRate'} = 'CouponPayment';
-pfCfTable.CouponPayment = pfCfTable.CouponPayment * 100;
-
-%% join with bond prices
-
-fullBondMarket = outerjoin(btPrices, pfCfTable, 'Keys', {'TreasuryID', 'Date'},...
-    'MergeKeys', true, 'Type', 'left');
-
-%% plot eligible bond prices and coupon payments
-
-% get only coupon payments
-xxInds = ~isnan(fullBondMarket.CouponPayment);
-eligCoupons = fullBondMarket(xxInds, :);
-
-subplot(2, 1, 1)
-plot(eligCoupons.Date, eligCoupons.CouponPayment, '.')
-datetick 'x'
-grid on
-grid minor
-xlabel('date')
-ylabel('Coupon payment')
-title('Coupon payments of eligible bonds')
-
-
-% plot eligible bonds
-
-widePrices = unstack(btPrices(:, {'Date', 'TreasuryID', 'Price'}), 'Price', 'TreasuryID');
-
-subplot(2, 1, 2)
-plot(widePrices.Date, widePrices{:, 2:end}, 'Color', 0.4*[1, 1, 1])
-datetick 'x'
-grid on
-grid minor
-xlabel('date')
-ylabel('price')
-title('Eligible bond prices')
-
-%% In 3D - rather useless
-
-yVals = repmat(1:size(widePrices(:, 2:end), 2), size(widePrices, 1), 1);
-plot3(widePrices.Date, yVals, widePrices{:, 2:end}, '.k')%, 'Color', 0.4*[1, 1, 1])
-camlight
-datetick 'x'
-grid on
-grid minor
-xlabel('date')
-ylabel('price')
-title('Eligible bond prices')
-
-
-%% visualize number of eligible bonds over time
-
-% get number of eligible bonds per date
-nEligibleBonds = grpstats(btPrices(:, {'Date', 'CurrentMaturity'}), 'Date');
-nEligibleBonds = sortrows(nEligibleBonds, 'Date');
-
-plot(nEligibleBonds.Date, nEligibleBonds.GroupCount, '.')
-grid on
-grid minor
-datetick 'x'
-xlabel('time')
-ylabel('Number of eligible bonds')
+% %% load data
+% 
+% % set data directory
+% dataDir = '../priv_bondPriceData';
+% fname = fullfile(dataDir, 'syntheticBondsLongFormat.mat');
+% load(fname)
+% 
+% %% define strategy parameters
+% 
+% GS = GlobalSettings;
+% 
+% % initial wealth
+% initWealth = 10000;
+% 
+% % transaction costs
+% transCosts = 10 / 10000;
+% 
+% % define initial starting date and move to next business day
+% desiredInitDate = datenum('1975-01-02');
+% initDate = makeBusDate(desiredInitDate, 'follow', GS.Holidays, GS.WeekendInd);
+% 
+% % define TTM range
+% minDur = 7*365 + 2; % exclude 7 year notes
+% maxDur = 10*365;
+% 
+% % define grid of desired maturities for initial portfolio allocation
+% maturGrid = datetime(datevec(initDate)) + calyears(7) + calmonths(3:3:36);
+% maturGrid = datenum(maturGrid);
+% 
+% %% restrict observations with regards to chosen backtest period
+% 
+% % get observations within backtest period
+% xxInd = longPrices.Date >= initDate;
+% btPrices = longPrices(xxInd, :);
+% 
+% % get time to maturity for each observation
+% btPrices = sortrows(btPrices, 'Date');
+% btPrices.CurrentMaturity = btPrices.Maturity - btPrices.Date;
+% 
+% % eliminate 30 year bonds
+% xxInds = strcmp(btPrices.TreasuryType, '30-Year BOND');
+% btPrices = btPrices(~xxInds, :);
+% 
+% % reduce to eligible bonds with small buffer
+% xxEligible = btPrices.CurrentMaturity >= (minDur - 5) & btPrices.CurrentMaturity <= maxDur;
+% btPrices = btPrices(xxEligible, :);
+% 
+% % get relevant quantities
+% bondMarket = btPrices(:, {'Date', 'TreasuryID', 'Price', 'Maturity', 'CouponPayment'});
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% start backtest
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% clear as much as possible
-clearvars -except eligibleBonds fullBondMarket GS initWealth initDate minDur maDur
-
-load devWkspace.mat
-
-% get relevant quantities
-bondMarket = fullBondMarket(:, {'Date', 'TreasuryID', 'Price', 'Maturity', 'CouponPayment'});
+load devWkspace
 
 %% pre-allocate bond portfolio book-keeping
 
@@ -178,35 +83,10 @@ pfHistory = cell2table(cell(0, length(colNames)), 'VariableNames', colNames);
 
 clearvars allDates xxVals colNames
 
-%% visualize problem of initial bond allocation
-
-thisDay = initDate;
-
-% define grid of desired maturities
-maturGrid = datetime(datevec(thisDay)) + calyears(7) + calmonths(3:3:36);
-maturGrid = datenum(maturGrid);
-
-% get prices
-singleDayPrices = selRowsProp(bondMarket, 'Date', thisDay);
-
-% visualize selection
-doPlotting = false;
-if doPlotting
-    plot(singleDayPrices.Maturity, 1:length(singleDayPrices.Maturity), '.')
-    hold on
-    ylim = get(gca, 'YLim');
-    for ii=1:length(maturGrid)
-        plot(maturGrid(ii)*[1 1], ylim, '-r')
-    end
-    datetick 'x'
-    xlabel('Maturity')
-    ylabel('Number of bonds')
-    title(['Desired maturities on ' datestr(thisDay)])
-    grid on
-    grid minor
-end
-
 %% select bond universe as bonds closest to desired maturities
+
+% get prices on first date
+singleDayPrices = selRowsProp(bondMarket, 'Date', initDate);
 
 % for each desired maturity, find closest bond
 bondMaturities = singleDayPrices.Maturity;
@@ -218,7 +98,6 @@ currentUniverse = sortrows(currentUniverse, 'Maturity');
 
 % get desired buy value for each asset
 nAss = size(currentUniverse, 1);
-transCosts = 0.3;
 cashLeft = initWealth;
 nRemaining = nAss;
 
@@ -232,7 +111,7 @@ for ii=1:nAss
     currPrice = currentUniverse.Price(ii);
     
     % add transaction costs
-    currTradePrice = currPrice + transCosts;
+    currTradePrice = currPrice * (1 + transCosts);
     thisTransactionPrices(ii, 1) = currTradePrice;
     
     % get desired value to buy
@@ -268,7 +147,13 @@ pfHistory = [pfHistory; initOrders];
 % - sell assets, attach orders and transaction prices to assets sold
 % - buy assets, attach orders and transaction prices to assets bought
 
-for ii=2:10000
+nObs = size(cashAccount, 1);
+for ii=2:nObs
+    if mod(ii, 1000) == 0
+        display(ii/nObs)
+    end
+    
+    
     % get current date
     thisDate = cashAccount.Date(ii);
     
@@ -289,7 +174,7 @@ for ii=2:10000
     
     % get full information on current portfolio
     currAssetsMarket = outerjoin(currPf(:, {'Date', 'TreasuryID', 'MorningVolumes', 'Orders'}), ...
-        bondMarket, 'Keys', {'Date', 'TreasuryID'},...
+        currBondMarket, 'Keys', {'Date', 'TreasuryID'},...
         'MergeKeys', true, 'Type', 'left');
     
     % get coupon payments
@@ -298,13 +183,13 @@ for ii=2:10000
     
     % find selling assets
     currAssetsMarket.TTM = currAssetsMarket.Maturity - currAssetsMarket.Date;
-    if any(currAssetsMarket.TTM < minDur + 5) % TRADE
+    if any(currAssetsMarket.TTM <= minDur) % TRADE
         % find asset to sell
-        indSell = currAssetsMarket.TTM < minDur + 5;
+        indSell = currAssetsMarket.TTM <= minDur;
         oldAssets = currAssetsMarket;
         oldAssets.TransactionPrices = oldAssets.Price;
         oldAssets.Orders(indSell) = (-1)*oldAssets.MorningVolumes(indSell);
-        oldAssets.TransactionPrices(indSell) = oldAssets.TransactionPrices(indSell) - transCosts;
+        oldAssets.TransactionPrices(indSell) = oldAssets.TransactionPrices(indSell) * (1 - transCosts);
         oldAssets.TTM = [];
         
         % compute cash that one gets from transactions
@@ -319,7 +204,7 @@ for ii=2:10000
         assetToBuy = currBondMarket(xxInd, :);
     
         % add transaction costs
-        currTradePrice = assetToBuy.Price + transCosts;
+        currTradePrice = assetToBuy.Price + (1 + transCosts);
         assetToBuy.TransactionPrices = currTradePrice;
         
         % get number of full units that can be bought
@@ -330,7 +215,19 @@ for ii=2:10000
         % write transaction costs into cash account
         cashAccount.Transactions(ii) = (-1)*(currTradePrice * nUnits) + cashFromSell;
         
-        currAssetsMarket = [oldAssets; assetToBuy];
+        % combine newly bought bond with already existing bonds
+        if ~ismember(assetToBuy.TreasuryID, oldAssets.TreasuryID)
+            currAssetsMarket = [oldAssets; assetToBuy];
+        else
+            % find new bond in old portfolio
+            xxInd = find(oldAssets.TreasuryID == assetToBuy.TreasuryID);
+            
+            % replace entry with new values except for volumes
+            xxVolume = oldAssets.MorningVolumes(xxInd);
+            oldAssets(xxInd, tabnames(assetToBuy)) = assetToBuy;
+            oldAssets.MorningVolumes(xxInd) = xxVolume;
+            currAssetsMarket = oldAssets;
+        end
         currAssetsMarket.Maturity = [];
         
     else % NO TRADE
@@ -339,6 +236,10 @@ for ii=2:10000
         currAssetsMarket.Properties.VariableNames{'Maturity'} = 'TransactionPrices';
         currAssetsMarket.TTM = [];
     end
+    
+    % check for non-unique assets in portfolio history
+    n1 = size(currAssetsMarket, 1);
+    n2 = length(unique(currAssetsMarket.TreasuryID));
     
     % attach orders to portfolio history
     pfHistory = [pfHistory; currAssetsMarket];
@@ -367,6 +268,17 @@ pfValues.FullValue = pfValues.MarketValue + pfValues.Cash;
 %%
 
 plot(pfValues.Date, pfValues.FullValue)
+datetick 'x'
+grid on
+grid minor
+
+%% 
+
+plot(pfValues.Date, pfValues.FullValue)
+hold on
+plot(pfValues.Date, pfValues.Cash)
+plot(cashAccount.Date(2:end), cumsum(cashAccount.Coupons(2:end)))
+hold off
 datetick 'x'
 grid on
 grid minor
